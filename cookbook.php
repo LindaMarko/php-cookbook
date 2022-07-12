37e4f721e6e102f62e6c9756e94b143149c5dbf4
Linda Marko <lindamarko83@gmail.com>
2022-07-12 11:38:25 +0200

    Add more getters and setters

--- cookbook.php
+++ cookbook.php
@@ -4,16 +4,31 @@ include('classes/recipes.php');
 
 
 $recipe1 = new Recipe();
-$recipe1->source = "Grandma Holligan";
+$recipe1->setSource = "Grandma Holligan";
 $recipe1->setTitle("my first recipe");
 $recipe1->addIngredient("egg", 1);
 $recipe1->addIngredient("flour", 2, "cup");
 
 $recipe2 = new Recipe();
-$recipe2->source = "Betty Crocker";
+$recipe2->setSource = "Betty Crocker";
 $recipe2->setTitle("My Second Recipe");
 
 echo $recipe1->getTitle();
 foreach($recipe1->getIngredients() as $ing) {
   echo "\n" . $ing["amount"] . " " . $ing["measure"] . " " . $ing["item"];
 }
+
+$recipe1->addInstruction("First instruction");
+$recipe1->addInstruction("Second instruction");
+
+echo implode("\n", $recipe1->getInstructions());
+
+$recipe1->addTag("Breakfast");
+$recipe1->addTag("Main Course");
+
+echo implode(", ", $recipe1->getTags());
+
+$recipe1->setYield("6 servings");
+
+echo  $recipe1->getYield();
+echo  $recipe1->getSource();
