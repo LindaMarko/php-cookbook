9705fe8c168c342234fec758e9d8ce0ce1107b13
Linda Marko <lindamarko83@gmail.com>
2022-07-13 08:44:33 +0200

    Add render.php with static methods

--- cookbook.php
+++ cookbook.php
@@ -1,7 +1,7 @@
 <?php
 
 include('classes/recipes.php');
-
+include('classes/render.php');
 
 $recipe1 = new Recipe();
 $recipe1->setSource = "Grandma Holligan";
@@ -13,22 +13,12 @@ $recipe2 = new Recipe();
 $recipe2->setSource = "Betty Crocker";
 $recipe2->setTitle("My Second Recipe");
 
-echo $recipe1->getTitle();
-foreach($recipe1->getIngredients() as $ing) {
-  echo "\n" . $ing["amount"] . " " . $ing["measure"] . " " . $ing["item"];
-}
-
 $recipe1->addInstruction("First instruction");
 $recipe1->addInstruction("Second instruction");
-
-echo implode("\n", $recipe1->getInstructions());
 
 $recipe1->addTag("Breakfast");
 $recipe1->addTag("Main Course");
 
-echo implode(", ", $recipe1->getTags());
-
 $recipe1->setYield("6 servings");
 
-echo  $recipe1->getYield();
-echo  $recipe1->getSource();
+echo Render::displayRecipe($recipe1);
