30395dc75077bca3e0855129a06e7cfb32f998bf
Linda Marko <lindamarko83@gmail.com>
2022-07-13 09:35:24 +0200

    Add magical methods: __constuct(), __toString()

--- cookbook.php
+++ cookbook.php
@@ -3,15 +3,13 @@
 include('classes/recipes.php');
 include('classes/render.php');
 
-$recipe1 = new Recipe();
+$recipe1 = new Recipe("my first recipe");
 $recipe1->setSource = "Grandma Holligan";
-$recipe1->setTitle("my first recipe");
 $recipe1->addIngredient("egg", 1);
 $recipe1->addIngredient("flour", 2, "cup");
 
 $recipe2 = new Recipe();
 $recipe2->setSource = "Betty Crocker";
-$recipe2->setTitle("My Second Recipe");
 
 $recipe1->addInstruction("First instruction");
 $recipe1->addInstruction("Second instruction");
